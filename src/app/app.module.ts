0b389857578f138738fa6b88a74698135cf2e5e6
Julien_B <julienbailly08@gmail.com>
2021-08-02 10:23:11 +0200

    add footer and sidePanel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidepanelComponent } from './sidepanel/sidepanel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidepanelComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
