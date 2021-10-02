7407f219690781898256746a35f7ea145c155958
Julien_B <julienbailly08@gmail.com>
2021-10-02 12:58:08 +0200

    update connexion to API

--- src/app/firstpage/firstpage.component.ts
+++ src/app/firstpage/firstpage.component.ts
@@ -7,15 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./firstpage.component.css']
 })
 export class FirstpageComponent implements OnInit {
-  
+
   response:any;
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<any>("http://localhost:8000/api/users/").subscribe(data=>{
+    /*this.http.get<any>("http://localhost:8000/api/users/").subscribe(data=>{
       this.response = data['hydra:member'];
-    });
-    
+    });*/
+    this.http.get<any>('http://127.0.0.1:8000/api/users.json').subscribe(
+      data => { this.response = data },
+      error => { console.log(error.status) }
+    );
+
   }
 
 }
